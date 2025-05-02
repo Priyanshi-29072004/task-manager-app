5a23f98218116b0e7bd5d4d407e6636e35d9f3ff
Priyanshi-29072004 <vaghasiapriyanshi@gmail.com>
2025-05-02 18:00:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/tasks", require("./routes/taskRoutes"));
 
